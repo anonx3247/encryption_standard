e4fffca04befc63bfa0af02c87bf117287659a6f
Tadros Eliott <eliott.tadros@student-cs.fr>
2025-05-15 16:21:11 +0200

    Forgot some new modifications

--- hash.py
+++ hash.py
@@ -1,9 +1,9 @@
 from sbox import sbox
 
 def chunk_binary(string, length):
-    return [string[i:length+i].zfill(length) for i in range(0, len(string), length)]
+    return [int(string[i:length+i].zfill(length), 2) for i in range(0, len(string), length)]
 
-def hash(m : int) -> int :
+def hash(message : int) -> int :
     """
     Calcule le hash d'un message de taille quelconque, selon la construction de Merkle–Damgård.
     Pour la fonction, nous utilisons une construction du type Davies–Meyer, avec la 
@@ -11,18 +11,18 @@ def hash(m : int) -> int :
     Le S initial est un entier de 64 bits généré aléatoirement.
     La taille des morceaux et le nombre de rounds sont paramétrables.
     """
-    part_size = 64
+    chunk_size = 64
     rounds = 5
 
-    binary_m = bin(m)[2:]
-    messages = chunk_binary(binary_m, part_size)
+    binary_message = bin(message)[2:]
+    message_chunks = chunk_binary(binary_message, chunk_size)
 
     state = 0b0111000110010001001111000000111011100001010010100101100001100010
-    for message in messages :
-        int_message = int(message, 2)
+    
+    for chunk in message_chunks :
         for _ in range(rounds) :
             # print(state)
-            state = sbox(int_message ^ state) ^ state
+            state = sbox(chunk ^ state) ^ state
         # final_message += bin(int_message)[2:]
 
     return state
