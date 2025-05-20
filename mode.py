b9be5c4f69de266944462abeff56e29728e09337
Anas Lecaillon <anas@lecaillon.com>
2025-05-20 18:02:35 +0200

    fixed a few bugs in mode of operation

--- mode.py
+++ mode.py
@@ -11,23 +11,27 @@ def encrypt_and_authenticate(keys: list[np.uint16], m: int, aux=None):
     # Chiffrement du message
     c = block_encrypt(k1, m)
 
+    k2 = int(k2)
+
     tag = hash(m ^ k2)
     
     return aux, c, tag
 
-def decrypt_and_verify(keys: list[np.uint16], c: int, tag: int):
+def decrypt_and_verify(keys: list[np.uint16], c: int, tag: int, aux=None):
     if len(keys) != 2:
         raise ValueError("Deux clés sont requises")
     
     k1, k2 = keys
+    k2 = int(k2)
+    
+    # Déchiffrement du message seulement si le tag est valide
+    m = block_decrypt(k1, c)
+
     
     # Vérification du tag avant déchiffrement
     computed_tag = hash(m ^ k2)
     
     if computed_tag != tag:
         return False, None
-    
-    # Déchiffrement du message seulement si le tag est valide
-    m = block_decrypt(k1, c)
     
     return True, m
